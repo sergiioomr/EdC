2a8592285349084aaa87dbafe31058efe08ccaa9
sergiioomr <alu0101718194@ull.edu.es>
2025-11-17 10:46:41 +0000

    Testbench comentado

--- SegundaPractica/Ficheros/microc_tb.v
+++ SegundaPractica/Ficheros/microc_tb.v
@@ -47,16 +47,25 @@ end
 // Bloque simulación señales control por ciclo
 initial
 begin
-  // retardos y señales para ejecutar primera instrucción (ciclo 1)
+  // retardos y señales para ejecutar primera instrucción (ciclo 1) (j start)
   #10
-  s_inc = 0;
-  s_inm = 0;
-  we3 = 0;
-  wez = 0;
-  Op = 3'b000;
+  s_inc = 0; // Al ser una instrucción j, hay que hacer un salto con una dirección absoluta
+  s_inm = 0; // No importa, no se va a escribir nada en el banco
+  we3 = 0; // No se escribe nada, no hay que activarlo
+  wez = 0; // No hay que escribir el flag de zero
+  Op = 3'b000; // No hay operacion
   #10
 
-  // retardos y señales para ejecutar segunda instrucción (ciclo 2)
+  // retardos y señales para ejecutar segunda instrucción (ciclo 2) (li #0 R2)
+  #10
+  s_inc = 1; // No se usan saltos, la siguiente dirección del PC es PC + 1
+  s_inm = 1; // Hay que usar un inmediato
+  we3 = 1; // Se necesita usar el banco, hay que habilitarlo
+  wez = 0; // No se usa el flag de zero
+  Op = 3'b000; // No se hay operación 
+  #10
+
+  // retardos y señales para ejecutar tercera instrucción (ciclo 3) (li #2 R1)
   #10
   s_inc = 1;
   s_inm = 1;
@@ -65,7 +74,7 @@ begin
   Op = 3'b000;
   #10
 
-  // retardos y señales para ejecutar tercera instrucción (ciclo 3)
+  // retardos y señales para ejecutar cuarta instrucción (ciclo 4) (li #4 R3)
   #10
   s_inc = 1;
   s_inm = 1;
@@ -74,7 +83,7 @@ begin
   Op = 3'b000;
   #10
 
-  // retardos y señales para ejecutar cuarta instrucción (ciclo 4)
+  // retardos y señales para ejecutar quintaW instrucción (ciclo 5) (li #1 R4)
   #10
   s_inc = 1;
   s_inm = 1;
@@ -83,25 +92,16 @@ begin
   Op = 3'b000;
   #10
 
-  // retardos y señales para ejecutar quintaW instrucción (ciclo 5)
+  // retardos y señales para ejecutar sexta instrucción (ciclo 6) (add R2 R3 R2)
   #10
-  s_inc = 1;
-  s_inm = 1;
-  we3 = 1;
-  wez = 0;
-  Op = 3'b000;
-  #10
-
-  // retardos y señales para ejecutar sexta instrucción (ciclo 6)
-  #10
-  s_inc = 1;
-  s_inm = 0;
-  we3 = 1;
-  wez = 1;
-  Op = 3'b010;
+  s_inc = 1; // Pc + 1
+  s_inm = 0; // se usa el resultado de la ALU
+  we3 = 1; // Hay que habilitar el banco
+  wez = 1; // Actualizamos el flag de zero tras la operación hecha
+  Op = 3'b010; // Opcode de add
   #10
   
-  // retardos y señales para ejecutar séptima instrucción (ciclo 7)
+  // retardos y señales para ejecutar séptima instrucción (ciclo 7) ()
   #10
   s_inc = 1;
   s_inm = 0;
